8ea01060f0f54ff73c68f3e949a4741261b3cb1c
Ewa Kwasnik <kwasnik.ewa@gmail.com>
2021-07-09 14:48:57 +0200

    add columns and cards

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import Column from '../Column/ColumnContainer.js';
-{/*import Creator from '../Creator/Creator.js';*/}
+import Creator from '../Creator/Creator.js';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
@@ -14,6 +14,7 @@ class List extends React.Component {
     image: PropTypes.string.isRequired,
     description: PropTypes.node,
     columns: PropTypes.array,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -21,7 +22,7 @@ class List extends React.Component {
   }
   
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} splashImage={image} />
@@ -33,9 +34,11 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/*<div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+        <div className={styles.creator}>
+          {/*<Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>*/}
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
+        </div>
       </section>
     );
   }
